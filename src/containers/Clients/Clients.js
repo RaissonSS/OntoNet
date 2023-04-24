e93faa29e9fe4eeefe789946486d28c435201ab1
Raisson Souza <raissonrai@gmail.com>
2023-04-23 21:52:08 -0300

    - Clients container adjusted. - New assets added

--- src/containers/Clients/Clients.js
+++ src/containers/Clients/Clients.js
@@ -3,13 +3,25 @@ import "./styles.css"
 
 import data from "../../data/index"
 
+import TechnologicalSphereImage from "../../assets/technological-sphere.png"
+import ClientImage from "../../assets/client-image.jpg"
+
 function Clients() {
     return (
         <div className="clients-box">
-            <p>{ data.clients[0] }</p>
-            <img alt="foto 01" />
-            <p>{ data.clients[0] }</p>
-            <img alt="foto 02" />
+            <div
+                className="clients-content"
+            >
+                <p>{ data.clients[0] }</p>
+                <img alt="client" src={ ClientImage } id="client-image" />
+            </div>
+            <div
+                className="clients-content"
+                style={ { flexDirection: "row-reverse" } }
+            >
+                <p>{ data.clients[0] }</p>
+                <img alt="sphere" src={ TechnologicalSphereImage } />
+            </div>
         </div>
     )
 }
