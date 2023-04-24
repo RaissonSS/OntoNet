d329a9c6a7a71dd5116cbf3d89bfcb935d0b441f
Raisson Souza <raissonrai@gmail.com>
2023-04-23 22:25:12 -0300

    - Contact container adjusted

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -4,13 +4,11 @@ import "./styles.css"
 function Contact() {
     return (
         <div className="contact-box">
-            <div className="content-contact">
-                <p>Entre em contato conosco através de nossas redes:</p>
-                <div className="contact-social-medias">
-                    <p><strong>Instagram:</strong>{ " @ontonet_oficial" }</p>
-                    <p><strong>WhatsApp:</strong>{ " +55 96784-2218" }</p>
-                    <p><strong>E-mail:</strong>{ " contato_ontonet@ontonet.com.br" }</p>
-                </div>
+            <p>Entre em contato conosco através de nossas redes:</p>
+            <div className="contact-social-medias">
+                <p><strong>Instagram:</strong>{ " @ontonet_oficial" }</p>
+                <p><strong>WhatsApp:</strong>{ " +55 96784-2218" }</p>
+                <p><strong>E-mail:</strong>{ " contato_ontonet@ontonet.com.br" }</p>
             </div>
         </div>
     )
